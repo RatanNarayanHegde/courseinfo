1ebc39f6893cddd6889be6035d6662a2cc24654f
RatanNarayanHegde <ratanhegde08@gmail.com>
2023-01-07 16:05:30 +0530

    add counter example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
-export default function App() {
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
   const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(0)}>reset</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
-}
+};
+
+export default App;
